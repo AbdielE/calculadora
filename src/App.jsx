dcdd2ecd72949dbba8935277917ebb7542daf00c
Abdiel Esteban Alfonso Chavez <isc19350558@gmail.com>
2023-03-09 09:51:13 -0600

    Sombra en botones

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <h1>Calculadora</h1>
+    <div className="app">
+      <h1 className="shadow-sm">Calculadora</h1>
       <table>
         {/* Primer fila*/}
         <tr>
-          <td colSpan={4}>Resultado</td>
+          <td colSpan={4} style={{
+            border:"1px solid black",
+            textAlign:"end"
+          }}>
+            <h2>0</h2>
+          </td>
         </tr>
         {/* Segunda fila*/}
         <tr>
@@ -115,10 +120,15 @@ function App() {
         </tr>
         {/* Sexta fila*/}
         <tr>
-          <td colSpan={2}>
+          <td>
             <button type="button"
-            className="btn btn-primary"
-            style={{width: "175px"}}>
+            className="btn btn-primary w-75">
+              {'<-'}
+            </button>
+          </td>
+          <td>
+            <button type="button"
+            className="btn btn-primary w-75">
               0
             </button>
           </td>
